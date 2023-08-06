36e21acc6c02f71b3514b6286d2b728331068e69
Jianeng Huang <jianengh@gmail.com>
2023-08-06 12:34:47 +0200

    add middleware and no cache fetch

--- utils/getDishes.ts
+++ utils/getDishes.ts
@@ -1,4 +1,7 @@
 export default async function getDishes() {
-	const response = await fetch(`/api/dish`);
+	const response = await fetch(
+		`http://${process.env.HOST}:${process.env.PORT}/api/dish`,
+		{ cache: 'no-cache' },
+	);
 	return new Response(response.body);
 }
