ddbc77a62950bd75be4dac1916204dc7ac0456aa
Jianeng Huang <jianengh@gmail.com>
2023-08-09 13:49:31 +0200

    Revert "remove api folder and calling api from function"
    This reverts commit b255890a5b8b98805ff137c015890bb46babfdae.

--- app/api/dish/route.ts
+++
@@ -1,9 +0,0 @@
-import db from 'db';
-import { NextResponse } from 'next/server';
-
-export const dynamic = 'force-dynamic';
-
-export async function GET() {
-	const dishes = await db.dish.findMany();
-	return NextResponse.json(dishes);
-}
